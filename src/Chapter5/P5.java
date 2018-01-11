0eebc2ddbffbdbcb367c14ea2528f2c54ce2677d
JoshuaAbles <ja0962305@RK03-B1225-08.francistuttle.edu>
2018-01-11 13:08:08 -0600

    Adds the chapter 5 and 6 programs

---
+++ src/Chapter5/P5.java
@@ -0,0 +1,14 @@
+package Chapter5;
+import java.util.Scanner;
+/**
+ * Program to count votes.
+ * 
+ * @author ja0962305
+ */
+public class P5 {
+    /**
+     * Main Method
+     *
+     * @param args arguments from command line prompt
+     */
+}
---
+++ src/Chapter6/P6.java
@@ -0,0 +1,14 @@
+package Chapter6;
+import java.util.Scanner;
+/**
+ * Program to convert currency.
+ * 
+ * @author ja0962305
+ */
+public class P6 {
+    /**
+     * Main Method
+     *
+     * @param args arguments from command line prompt
+     */
+}
