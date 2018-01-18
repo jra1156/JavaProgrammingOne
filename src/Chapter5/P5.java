14ced7fafec8c1299172c2e0908c8edb453d6441
JoshuaAbles <ja0962305@RK03-B1225-08.francistuttle.edu>
2018-01-18 12:45:36 -0600

    First Version of the files

--- src/Chapter5/P5.java
+++ src/Chapter5/P5.java
@@ -11,4 +11,28 @@ public class P5 {
      *
      * @param args arguments from command line prompt
      */
+public static void main(String[] args) {
+        int yes = 0;
+        int no = 0;
+        String n="";
+        Scanner input = new Scanner(System.in);
+        do{
+        System.out.print(" do you vote yes or no? enter Y for yes, N for no, or press Q to exit: ");
+        n = input.next();
+        if( n.equalsIgnoreCase("Y"))
+        {
+            yes +=1;
+        }
+        else if (n.equalsIgnoreCase("N"))
+        {
+            no += 1;
+        }
+        else if (n.equalsIgnoreCase("Q"))
+        {
+            System.out.print(" the total number of yeses is " + yes +" and the total number of nos is " + no +"");
+        }
+        else
+            System.out.print("Invalid");
+        } while (!n.equalsIgnoreCase("Q"));
+    }
 }
--- src/Chapter6/P6.java
+++ src/Chapter6/P6.java
@@ -1,3 +1,4 @@
+
 package Chapter6;
 import java.util.Scanner;
 /**
@@ -11,4 +12,76 @@ public class P6 {
      *
      * @param args arguments from command line prompt
      */
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(" Enter how much euros a dollar will buy: ");
+        double buy1 = input.nextDouble();
+        System.out.println(" Enter how much pound sterling a dollar will buy: ");
+        double buy2 = input.nextDouble();
+        System.out.println(" Enter how much Yen a dollar will buy: ");
+        double buy3 = input.nextDouble();
+        String answer;
+
+        do {
+            System.out.println(" enter how much money in dollars you have : ");
+            double dol = input.nextDouble();
+            System.out.println(" enter E to change the amount to euros, P to change the amount to pound sterling or Y to change the amount into yen: ");
+            String a = input.next();
+            
+            char b;
+            b = a.charAt(0);
+            if (b == 'E') {
+                double Euro = Convert(dol, buy1);
+                System.out.printf("For %.2f dollars you will get %.2f euros", dol, Euro);
+            }
+            else if (b == 'e') {
+                double Euro = Convert(dol, buy1);
+                System.out.printf("For %.2f dollars you will get %.2f euros", dol, Euro);
+            }
+
+            else if (b == 'P') {
+                double Pound = Convert(dol, buy2);
+                System.out.printf("For %.2f dollars you will get %.2f yen", dol, Pound);
+            }
+            else if (b == 'p') {
+                double Pound = Convert(dol, buy2);
+                System.out.printf("For %.2f dollars you will get %.2f yen", dol, Pound);
+            }
+
+            else if (b == 'Y') {
+                double Yen = Convert(dol, buy3);
+                System.out.printf("For %.2f dollars you will get %.2f yen", dol, Yen);
+            }
+            else if (b == 'y') {
+                double Yen = Convert(dol, buy3);
+                System.out.printf("For %.2f dollars you will get %.2f yen", dol, Yen);
+            }
+            do{
+            System.out.println(" do you to continue? enter yes to contiune or no to end the program: ");
+            answer = input.next();
+             } while (!answer.equalsIgnoreCase("no") && !answer.equalsIgnoreCase("yes"));
+        } while (!answer.equalsIgnoreCase("no"));
+    }
+        
+
+    public static double Convert(double dol, double buy) {
+        
+        double reduction = 0.95;
+        double bigreduction = .9;
+        double dolls = 0;
+        double total; 
+        
+        if (dol > 100) {
+            dolls = dol * reduction;          
+            total = dolls * buy;
+
+        } else{
+            dolls = dol * bigreduction;
+            total = dolls * buy;
+
+        }
+        return total;
+
+    }
+        
 }
